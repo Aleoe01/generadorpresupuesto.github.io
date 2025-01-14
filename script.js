4db1dbb47ce84d41de96a9a3f7247936a874614f
Aleoe01 <aleoe.12@gmail.com>
2025-01-14 13:20:33 -0300

    index y boton enviar terminados

---
+++ script.js
@@ -0,0 +1,62 @@
+
+const formaPago = document.getElementById("formaPago");
+const cantidadCuotas = document.getElementById("cantidadCuotas");
+
+// Mostrar u ocultar las cuotas dependiendo de la forma de pago
+formaPago.addEventListener('change', function () {
+    if (formaPago.value === "contado") {
+        cantidadCuotas.style.display = "none";
+    } else {
+        cantidadCuotas.style.display = "block";
+    }
+});
+
+
+document.getElementById('enviar').addEventListener('click', function() {
+    
+    // Obtener los datos del formulario
+
+    const nombreVendedor = document.getElementById("nombreVendedor").value;
+    const nombreCliente = document.getElementById("nombreCliente").value;
+    const telefono = document.getElementById("telefono").value;
+    const producto = document.getElementById("producto").value;
+    const precio = document.getElementById("precio").value;
+    const formaPago = document.getElementById("formaPago").value;
+    const cantidadCuotas = document.getElementById("cantidadCuotas").value;
+
+    // Crear el mensaje dependiendo de la forma de pago
+
+    let mensaje = "";
+
+    if (formaPago === "contado") {
+        mensaje =
+            "Hola " +
+            nombreCliente +
+            ", soy "+ nombreVendedor +" de On City Esperanza! Te escribo porque sé que has estado interesado/a en el " +
+            producto +
+            " y ahora es el momento ideal para aprovecharlo. Tengo un precio contado especial de $ " +
+            precio +
+            " disponible por tiempo limitado.\nSi te interesa, házmelo saber y te doy todos los detalles. ¡No dejes pasar esta oportunidad!";
+    } else if (formaPago === "tb") {
+        mensaje =
+            "Hola " +
+            nombreCliente +
+            ", soy Ale de On City Esperanza! Te escribo porque sé que has estado interesado/a en el " +
+            producto +
+            ". El precio es de $ " +
+            precio +
+            " y podemos ofrecerte el pago en " +
+            cantidadCuotas +
+            " cuotas con tu tarjeta de credito.\nSi te interesa, házmelo saber y te doy todos los detalles. ¡No dudes en consultarme!";
+    }
+
+    // Codificar el mensaje para URL
+    const encodedMessage = encodeURIComponent(mensaje);
+
+    // Crear el enlace de WhatsApp
+    const whatsappURL = "https://wa.me/549" + telefono + "?text=" + encodedMessage;
+
+    // Abrir WhatsApp en una nueva pestaña
+    window.open(whatsappURL, "_blank");
+
+} )
